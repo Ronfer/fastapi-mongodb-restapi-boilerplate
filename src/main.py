b9bdbd7ee77a0e5e4a4754c7cf0e861cc6c3ef7c
Ronnie Isomäki <42524219+Ronfer@users.noreply.github.com>
2021-11-20 12:05:37 +0200

    Add CORSMiddleware boilerplate for future

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
 from .routes import post, user, auth
 
+# allowed_urls = ["*"]
+# allowed_urls = ["https://www.google.com"]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=allowed_urls,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app = FastAPI()
 app.include_router(post.router)
